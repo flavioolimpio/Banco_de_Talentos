c10ff2bb25d17dfa4e0302a0d4d22fc1faad895f
Flavio <flavio_olimpio@outlook.com>
2026-05-26 20:04:37 -0300

    fix: usar CompressedStaticFilesStorage sem manifesto no servidor

--- django_app/config/settings/server.py
+++ django_app/config/settings/server.py
@@ -5,6 +5,11 @@ from .base import *  # noqa: F403
 
 DEBUG = False
 
+STORAGES = {
+    "default": {"BACKEND": "django.core.files.storage.FileSystemStorage"},
+    "staticfiles": {"BACKEND": "whitenoise.storage.CompressedStaticFilesStorage"},
+}
+
 SECURE_CONTENT_TYPE_NOSNIFF = True
 SECURE_REFERRER_POLICY = "same-origin"
 X_FRAME_OPTIONS = "DENY"
